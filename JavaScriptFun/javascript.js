b67e3484fb41709a6f8cda664431ce23a3d5ae1b
raphsutti <neoster9@gmail.com>
2017-12-03 19:12:43 +1100

    More JavaScript functions

--- JavaScriptFun/javascript.js
+++ JavaScriptFun/javascript.js
@@ -1,3 +1,4 @@
+// simple function
 function go(name, age) {
 	if (age < 20) {
 		return name+ '!';
@@ -5,12 +6,45 @@ function go(name, age) {
 		return name
 	}
 }
+// alert( go('Raph', 19) );
 
-alert( go('Raph', 19) );
-
-
+// simple add function
 function add(first, second) {
 	return first+second;
 }
+// alert( "1+2 is " + add(1,2));
 
-alert( "1+2 is " + add(1,2));
+// array manipulation
+var myList = ['apples', 'oranges', 'bananas'];
+myList.push('pineapples');
+myList.shift();
+myList.pop();
+
+// forEach method
+var myList2 = ['apples', 'oranges'];
+console.log(myList2);
+myList2.forEach(function(value, index) {
+	console.log(value, index);
+	alert('I have '+value+' in my shopping bag');
+});
+
+// Loops: while do for
+// while loop
+var times = 0;
+while (times < 4) {
+	console.log('tried while', times);
+	times++;
+}
+
+// do while loop (guaranteed one execution)
+times = 0;
+do {
+	console.log('tried do', times);
+	times++;
+} while(times <2);
+
+// for loop
+var myList3 = ['Apples', 'Oranges', 'Bananas'];
+for (var i=0; i<myList3.length; i++) {
+	console.log('Current item is', myList3[i]);
+}
