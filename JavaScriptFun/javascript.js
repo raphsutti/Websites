b235debf3aa298c4a5d7f82bc1d538a004597553
raphsutti <neoster9@gmail.com>
2017-12-03 18:49:19 +1100

    Using Javascript in a webpage

---
+++ JavaScriptFun/javascript.js
@@ -0,0 +1,16 @@
+function go(name, age) {
+	if (age < 20) {
+		return name+ '!';
+	} else {
+		return name
+	}
+}
+
+alert( go('Raph', 19) );
+
+
+function add(first, second) {
+	return first+second;
+}
+
+alert( "1+2 is " + add(1,2));
