592512b733839e199e0fd23b5ccd9fd9135f58d8
raphsutti <neoster9@gmail.com>
2017-12-05 23:09:35 +1100

    Playing with JQuery

---
+++ jqueryTest/javascript.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	// $("#panel1").hide(500).show(500).slideUp(1000).delay(200).slideDown(1000);
+	// $("#panel2").fadeToggle(2500).fadeToggle(2500);
+	$("#panel1").css({
+		color:"red", 
+		fontWeight:'bold'
+	});
+
+	$("#panel2").css('opacity','0.5');
+
+});
