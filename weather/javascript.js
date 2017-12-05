a21193151dac8c7e13c8b23879ba22c0a81101e4
raphsutti <neoster9@gmail.com>
2017-12-05 22:31:01 +1100

    Added icon and background to change depending on weather

--- weather/javascript.js
+++ weather/javascript.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
 	var fTemp
 	var weather;
 	var tempSwap = true;
+	var wind;
 	// Get user current location
 	if (navigator.geolocation) {
   		navigator.geolocation.getCurrentPosition(function(position) {
@@ -21,10 +22,28 @@ $(document).ready(function(){
 				cTemp = kTemp - 273.15;
 				fTemp = 9*(kTemp-273.15)/5 + 32;
 				weather = data.weather[0].main;
+				wind = data.wind.speed * 3.6;
 				$("#location").html(location);
 				$("#cTemp").html(cTemp.toFixed(1) + " &#8451");
 				$("#weather").html(weather);
-				// $("#fTemp").html(fTemp.toFixed(2) + " degress fahrenheit");
+				$("#wind").html(wind +" km/hr wind");
+
+				// change icons/background
+				if (weather ==="Rain" || weather ==="Drizzle") {
+					$("#icon").attr("class", "wi wi-showers")
+				} else if (cTemp >30) {
+					$("body").css('background-image', 'url(https://cdn.pixabay.com/photo/2015/06/20/07/24/color-815552_1280.png)');
+					$("#icon").attr("class", "wi wi-hot")
+				} else if (weather === "Clouds" || cTemp >10) {
+					$("body").css('background-color', '#34ABFF');
+					$("body").css('background-image', 'url(#)');
+					$("#icon").attr("class", "wi wi-day-cloudy")
+				} else if (cTemp >0) {
+					$("body").css('background-image', 'url(http://eskipaper.com/images/ice-background-2.jpg)');
+					$("#icon").attr("class", "wi wi-snowflake-cold")
+				}
+
+				// temp unit switch
 				$("#tempSwapBtn").click(function(){
 					if(tempSwap) {
 						$("#cTemp").html(fTemp.toFixed(1) + " &#8457");
