0016abb70f62804883929e8518494fbd1a5e98cf
raphsutti <neoster9@gmail.com>
2017-12-06 21:03:50 +1100

    Added jquery button events

--- jqueryTest/javascript.js
+++ jqueryTest/javascript.js
@@ -6,6 +6,26 @@ $(document).ready(function() {
 		fontWeight:'bold'
 	});
 
+	// modified looks of panel and button
 	$("#panel2").css('opacity','0.5');
+	$("#btn1").html('Changed Button 1')
+	
+	// add clicking functionality
+	$("#btn1").on('click', function() {
+		$('#panel1').toggle();
+	});
+
+	$("#btn2").on('mouseover', function() {
+		$('#panel2').slideToggle(200);
+	});
+
+	$("#btn3").on('mouseover', function() {
+		$('#panel3').fadeToggle(200);
+	});
+
+	$("#btn4").on('click', function() {
+		$('#panel4 .panel-body').html('Changed content');
+	});
+
 
 });
