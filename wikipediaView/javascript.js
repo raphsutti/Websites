20130cd1ef124fccb9933ca3a187f04af48fd983
raphsutti <neoster9@gmail.com>
2017-12-06 23:44:49 +1100

    search bar

--- wikipediaView/javascript.js
+++ wikipediaView/javascript.js
@@ -1,9 +1,10 @@
-var usrInput = document.getElementById('wikiSearch');
+// var usrInput = document.getElementById("wikiSearch").value;
+document.getElementById("wikiSearch").value = "Hello!"
 
-usrInput.addEventListener("input", check);
+// usrInput.addEventListener("input", check);
 
-function check() {
-	var result = usrInput.value;
-	// console.log(result);
-}
+// function check() {
+// 	// var result = usrInput.value;
+// 	console.log(usrInput);
+// }
 
