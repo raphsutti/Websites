ced086191c2503ca85b2df18d7d03ae811547a98
raphsutti <neoster9@gmail.com>
2017-12-06 21:41:29 +1100

    DRY programming style for jquery button behavior

--- jqueryTest/javascript.js
+++ jqueryTest/javascript.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	// $("#panel1").hide(500).show(500).slideUp(1000).delay(200).slideDown(1000);
 	// $("#panel2").fadeToggle(2500).fadeToggle(2500);
 	$("#panel1").css({
-		color:"red", 
+		color:"#FD7715", 
 		fontWeight:'bold'
 	});
 
@@ -11,21 +11,24 @@ $(document).ready(function() {
 	$("#btn1").html('Changed Button 1')
 	
 	// add clicking functionality
-	$("#btn1").on('click', function() {
-		$('#panel1').toggle();
+	$(".panel-button").on('mouseover', function() {
+		var panelId = '#'+$(this).attr('data-panelid'); // #panel*
+		var content = "changed content!";
+		$(panelId).fadeToggle();
+		$(panelId+' .panel-body').html(content);
 	});
 
-	$("#btn2").on('mouseover', function() {
-		$('#panel2').slideToggle(200);
-	});
+	// $("#btn2").on('mouseover', function() {
+	// 	$('#panel2').slideToggle(200);
+	// });
 
-	$("#btn3").on('mouseover', function() {
-		$('#panel3').fadeToggle(200);
-	});
+	// $("#btn3").on('mouseover', function() {
+	// 	$('#panel3').fadeToggle(200);
+	// });
 
-	$("#btn4").on('click', function() {
-		$('#panel4 .panel-body').html('Changed content');
-	});
+	// $("#btn4").on('click', function() {
+	// 	$('#panel4 .panel-body').html('Changed content');
+	// });
 
 
 });
