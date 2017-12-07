e1dca5bcd6a56457b3ea091d87840d35b6b241be
raphsutti <neoster9@gmail.com>
2017-12-07 22:36:29 +1100

    created wiki url link

--- wikipediaView/javascript.js
+++ wikipediaView/javascript.js
@@ -1,10 +1,34 @@
-// var usrInput = document.getElementById("wikiSearch").value;
-document.getElementById("wikiSearch").value = "Hello!"
+$(document).ready(function(){
+	//Default input
+	document.getElementById("wikiSearch").value = "Type your query here"
+	// wikiSearch.addEventListener("input", check);
+	// function check() {
+	// 	var result = wikiSearch.value;
+	// }
 
-// usrInput.addEventListener("input", check);
+	$("#wikiSearch").click(function(){
+		document.getElementById("wikiSearch").value = "";
+	})
+	$('#searchbtn').click(function(){
+		var wikiSearch = $('#wikiSearch').val();
+		console.log(wikiSearch);
+		var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+wikiSearch+"&callback=?";
+		console.log(url)
+		// $.ajax({
+		// 	type:"GET",
+		// 	url:url,
+		// 	async:false,
+		// 	dataType: "json",
+		// 	success: function(data){
+		// 		console.log(data)
+		// 	},
+		// 	error: function(errorMessage){
+		// 		alert("Error");
+		// 	}
+		// }
 
-// function check() {
-// 	// var result = usrInput.value;
-// 	console.log(usrInput);
-// }
+	})
+})
+
+
 
