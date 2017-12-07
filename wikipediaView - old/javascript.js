b64e24d4e01e6d9ca41c98d5aa57e973991a7cc7
raphsutti <neoster9@gmail.com>
2017-12-07 18:41:10 +1100

    backup with navbar

---
+++ wikipediaView - old/javascript.js
@@ -0,0 +1,10 @@
+// var usrInput = document.getElementById("wikiSearch").value;
+document.getElementById("wikiSearch").value = "Hello!"
+
+// usrInput.addEventListener("input", check);
+
+// function check() {
+// 	// var result = usrInput.value;
+// 	console.log(usrInput);
+// }
+
