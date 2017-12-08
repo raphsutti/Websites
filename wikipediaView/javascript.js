c480f11f766031f95e96a6680f4526bc92136224
raphsutti <neoster9@gmail.com>
2017-12-08 22:48:39 +1100

    added ajax call and link with search bar

--- wikipediaView/javascript.js
+++ wikipediaView/javascript.js
@@ -1,31 +1,38 @@
 $(document).ready(function(){
 	//Default input
-	document.getElementById("wikiSearch").value = "Type your query here"
+	// document.getElementById("wikiSearch").value = "Type your query here"
+	
 	// wikiSearch.addEventListener("input", check);
 	// function check() {
 	// 	var result = wikiSearch.value;
 	// }
 
-	$("#wikiSearch").click(function(){
-		document.getElementById("wikiSearch").value = "";
-	})
+	// Make default input disappear when clicked
+	// $("#wikiSearch").click(function(){
+	// 	document.getElementById("wikiSearch").value = "";
+	// })
+
 	$('#searchbtn').click(function(){
 		var wikiSearch = $('#wikiSearch').val();
-		console.log(wikiSearch);
-		var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+wikiSearch+"&callback=?";
-		console.log(url)
-		// $.ajax({
-		// 	type:"GET",
-		// 	url:url,
-		// 	async:false,
-		// 	dataType: "json",
-		// 	success: function(data){
-		// 		console.log(data)
-		// 	},
-		// 	error: function(errorMessage){
-		// 		alert("Error");
-		// 	}
-		// }
+		// console.log(wikiSearch);
+		var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+wikiSearch+"&format=json&callback=?";
+		// console.log(url)
+
+		// ajax call
+		$.ajax({
+			type:"GET",
+			url: url,
+			async: false,
+			dataType: "json",
+			success: function(data){
+				console.log(data[1][0]);
+				console.log(data[2][0]);
+				console.log(data[3][0]);
+			},
+			error: function(errorMessage){
+				alert("Error");
+			}
+		});
 
 	})
 })
