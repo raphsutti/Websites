8af4c2e8e23d5cc826c4588545d4b46301c78304
raphsutti <neoster9@gmail.com>
2017-12-19 20:00:26 +1100

    reset timer function

--- pomodoro/javascript.js
+++ pomodoro/javascript.js
@@ -13,18 +13,46 @@ $(document).ready(function(){
 		function timer() {
 			// hide variables
 			$("#start, #minus5Clock, #add5Clock, #add5Break, #minus5Break, #breakNum, #title1, #title2").hide();
+			$("#timeType").show();
 			$("#timeType").html("Session Time:");
 			count -= 1;
 			// stop at 0
 			if(count === 0 ) {
 				buzzer.play();
 				clearInterval(counter);
+				var startBreak = setInterval(breakTimer, 1000);
+				$("#num").hide();
 			}
 			$("#num").html(count);
 
+			function breakTimer() {
+				$("#timeType").html("Break Time:");
+				$("#breakNum").show();
+				breakTime -= 1;
+				if(breakTime === 0) {
+					clearInterval(startBreak);
+					buzzer.play();
+					$("#reset").show();
+					$("#breakNum, #timeType").hide();
+				}
+				$("#breakNum").html(breakTime);
+			}
+
 		}
+
+	});
+
+	// reset button
+	$("#reset").click(function() {
+		count = 25;
+		breakTime = 25;
+		$("#num").html(count);
+		$("#breakNum").html(breakTime);
+		$("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #num, #title1, #title2").show()
+		$("#timeType, #reset").hide();
 	})
 
+	// time adjustment buttons
 	$("#minus5Clock").click(function(){
 		if(count>5){
 			count -=5;
