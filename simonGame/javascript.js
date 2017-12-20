ef5d5ea88ef62136d1c4f9111638a2cf0ad2e811
raphsutti <neoster9@gmail.com>
2017-12-20 21:46:41 +1100

    added sounds and game engine

--- simonGame/javascript.js
+++ simonGame/javascript.js
@@ -1,25 +1,82 @@
-const RED = "RED";
-const BLUE = "BLUE";
-const YELLOW = "YELLOW";
-const GREEN = "GREEN";
+const red = "red";
+const blue = "blue";
+const yellow = "yellow";
+const green = "green";
 
 var simon = {
 	sendColor: function(color) {
+		if(!simon.sequence.length){
+			// start a new game
+			simon.nextSequence();
+		} else {
+			if(color === simon.sequence[simon.step]){
+				// go to next step
+				// test if last item
+				if(simon.step === simon.sequence.length-1) {
+				  console.log("sequence complete")
+				  simon.step = 0;
+				  simon.nextSequence();
+				} else {
+				  simon.step++;
+				}
+			} else {
+				// lose condition
+				buzz.play();
+				alert('Wrong!');
+				simon.sequence = []
+				simon.step = 0;
+				$("#start").show();
+			}
+		}
 		console.log("NEW COLOR: " + color);
+  	},
+	sequence: [],
+	colors: [red, blue, yellow, green],
+	step: 0,
+	nextSequence: function(){
+		var nextColor = simon.colors[Math.floor(Math.random() * simon.colors.length)]
+		// console.log("The random color is ", nextColor);
+		simon.sequence.push(nextColor);
+		console.log("the sequence ", simon.sequence);
+		for (var i=0; i<simon.sequence.length; i++) {
+
+			// $("#"+simon.sequence[i]).css("border", "5px solid black");
+			// $("#"+simon.sequence[i]).button('toggle');
+			buzz.play();
+			// $("#"+simon.sequence[i]).css("border", "");
+			// $("#"+simon.sequence[i]).removeClass("btn-secondary");
+		};
 	}
 }
-$(document).ready(function(){
-	// var buzzer = $('#buzzer')[0];
-	$("#red").click(function(){
-		simon.sendColor(RED)
+
+$(document).ready(function() {
+	var click = $("#click")[0];
+	var wrong = $("#wrong")[0];
+	var buzz = $("#buzz")[0];
+	$("#start").click(function(){
+		click.play();
+		$("#start").hide();
+		// $("#start").fadeOut(250);
+		// $("#start").fadeIn(250);
+		simon.nextSequence();
 	});
-	$("#blue").click(function(){
-		simon.sendColor(BLUE)
+	$("#blue").click(function() {
+		click.play();
+		simon.sendColor(blue);
+
 	});
-	$("#yellow").click(function(){
-		simon.sendColor(YELLOW)
+	$("#red").click(function() {
+		click.play();
+		simon.sendColor(red);
 	});
-	$("#green").click(function(){
-		simon.sendColor(GREEN)
+	$("#yellow").click(function() {
+		click.play();
+		simon.sendColor(yellow);
 	});
-});
+	$("#green").click(function() {
+		click.play();
+		simon.sendColor(green);
+	});
+  
+  
+})
